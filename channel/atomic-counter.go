e6243460e0a237526f8fc372ff7cbd001a331209
naecoo <ocean7hy@gmail.com>
2023-12-08 23:32:00 +0800

    channel finish

---
+++ channel/atomic-counter.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+func main() {
+	var ops atomic.Uint64
+	var val int64
+
+	var wg sync.WaitGroup
+
+	for i := 0; i < 50; i++ {
+		wg.Add(1)
+
+		go func() {
+			for c := 0; c < 1000; c++ {
+				val = val + 1
+				ops.Add(1)
+			}
+
+			wg.Done()
+		}()
+	}
+
+	wg.Wait()
+
+	fmt.Println("ops:", ops.Load())
+	fmt.Println("val:", val)
+}
