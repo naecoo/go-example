951634024ec6c5be1525a0ee5bf97e9882a63088
naecoo <ocean7hy@gmail.com>
2023-12-09 16:47:22 +0800

    go stl

---
+++ stl/line-filter.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+
+		ucl := strings.ToUpper(scanner.Text())
+
+		os.Stdout.Write([]byte(ucl + "\n"))
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "error:", err)
+		os.Exit(1)
+	}
+}
